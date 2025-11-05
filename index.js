40795d69b6dd6bb1c10ab9631e7d74779eee4cee
Andabeat <romina@nexiadesign.com>
2025-11-05 00:11:02 -0800

    Undo button added to counter

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let countEl = document.getElementById("count-el")
 let totalEl = document.getElementById("total-el")
 let count = 0
 let total = 0
+let saves = []  // keep a history of saves
+
 
 
 function increment() {
@@ -11,8 +13,10 @@ function increment() {
 }
 
 function save() {
+    saves.push(count); // Store the current count
     let countStr = count + " - "
-    saveEl.textContent += countStr
+    //saveEl.textContent += countStr
+    saveEl.textContent = saves.map(n => n + " - ").join("");
 
     total += count
     totalEl.textContent = "Total: " + total
@@ -21,3 +25,16 @@ function save() {
     count = 0
 }
 
+function undo () {
+    //remove last entry
+    let last = saves.pop();
+
+    //update the history display
+    saveEl.textContent = saves.map(n => n + " - ").join("");
+
+    //update total, only if there was a save undo
+   if (last !== undefined) {
+        total -= last;
+        totalEl.textContent = "Total: " + total;
+   }
+}
